5d8886033152a9e78e5dd7e63e8f400cd045e6ec
dnwn-9001 <j10511@hanmail.net>
2023-08-22 02:19:33 +0900

    SPA 폴더 구조 잡기

---
+++ src/js/pages/TechPage.js
@@ -0,0 +1,17 @@
+const TechContent = () => {
+  return `
+    <div id="list">
+        <p class="list__title"></p>
+        <div class="list__box">
+        <img class="card__img" src="" alt="thumbnail" srcset="" />
+        <div class="card__contents">
+            <p class="card__title"></p>
+            <p class="card__description"></p>
+            <p class="card__date"></p>
+        </div>
+        </div>
+    </div>
+    `;
+};
+
+export default TechContent;
