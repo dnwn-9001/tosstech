70f605189b60cd379330f9f8a598138f9dda818f
dnwn-9001 <j10511@hanmail.net>
2023-08-27 19:26:10 +0900

    더미 데이터 생성

--- src/js/components/TechComponent.js
+++ src/js/components/TechComponent.js
@@ -1,6 +1,43 @@
 // design component에 재활용 (이름 변경하기=>  ListComponent)
 const TechContent = () => {
+  const fetchDataAndRendering = () => {
+    fetch("src/json/data.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const listData = data;
+        renderListBox(listData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const renderListBox = (data) => {
+    const listUl = document.querySelector(".list__ul");
+    const listBox = data.map(
+      (item) =>
+        `
+        <a class="list__anchor">
+              <div class="list__box">
+                  <img class="card__img" src="${item.thumbnail}" alt="thumbnail"  />
+                  <div class="card__contents">
+                      <h1 class="card__title">${item.title}</h1>
+                      <p class="card__description">${item.description}</p>
+                      <p class="card__date">${item.date}</p>
+                  </div>
+              </div>
+        </a>
+      `
+    );
+
+    listUl.innerHTML = listBox;
+  };
+
   document.addEventListener("DOMContentLoaded", () => {
+    fetchDataAndRendering();
+
     const anchor = document.querySelector(".list__anchor");
     const thumbnail = document.querySelector(".card__img");
     const title = document.querySelector(".card__title");
@@ -21,16 +58,6 @@ const TechContent = () => {
         <h1 class="list__title">개발</h1>
         <div>
             <ul class="list__ul">
-                <a class="list__anchor">
-                    <div class="list__box">
-                        <img class="card__img" src="" alt="thumbnail" srcset="https://static.toss.im/assets/toss-tech/slash_juneseokbeomgun_thumb.png" />
-                        <div class="card__contents">
-                            <h1 class="card__title">레고처럼 조립하는 토스 앱</h1>
-                            <p class="card__description">수많은 서비스를.....</p>
-                            <p class="card__date">2023.8.22</p>
-                        </div>
-                    </div>
-                </a>
             </ul>
         </div>
     </div>
