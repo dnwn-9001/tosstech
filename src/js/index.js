636e9ba110122b80f949376263ec8fd39834ef24
dnwn-9001 <j10511@hanmail.net>
2023-09-01 04:25:33 +0900

    이름 변경

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,17 @@
 import createRouter from "./router.js";
-import NavContent from "./layout/Nav.js";
-import SectionContent from "./layout/Section.js";
-import FooterContent from "./layout/Footer.js";
-import MainContent from "./components/Main.js";
+import Nav from "./layout/Nav.js";
+import Section from "./layout/Section.js";
+import Footer from "./layout/Footer.js";
+import Main from "./components/Main.js";
 
 const nav = document.querySelector("nav");
 const container = document.querySelector("main");
 const section = document.querySelector("section");
 const footer = document.querySelector("footer");
 
-nav.innerHTML = NavContent();
-section.innerHTML = SectionContent();
-footer.innerHTML = FooterContent();
+nav.innerHTML = Nav();
+section.innerHTML = Section();
+footer.innerHTML = Footer();
 
 let page;
 
@@ -19,12 +19,12 @@ const pages = {
   tech: () => {
     page = "tech";
     container.textContent = "";
-    container.appendChild(MainContent(page));
+    container.appendChild(Main(page));
   },
   design: () => {
     page = "design";
     container.textContent = "";
-    container.appendChild(MainContent(page));
+    container.appendChild(Main(page));
   },
 };
 
