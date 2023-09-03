09697aa3d062b79cd8c6aee36249a87621f032ce
Hun Kang <hunni.devteam@gmail.com>
2023-09-03 21:12:27 +0900

    add: hot reload 설정
    - JSON static file fetch 오류 수정
    - add: styled element 예시 제공
    - Client-Side Router 설정 및 history api 테스트 예시

--- src/js/components/Detail.js
+++ src/js/components/Detail.js
@@ -1,10 +1,7 @@
-// import styled from "styled-components";
-// const articlelWrap = styled.div``;
-
 const Detail = (articleNo, dataUrl) => {
   const getData = async (articleNo) => {
     try {
-      const json = await (await fetch(`../../json/${dataUrl}.json`)).json();
+      const json = await (await fetch(`json/${dataUrl}.json`)).json();
       renderDetail(json, articleNo);
     } catch (err) {
       console.log(err);
@@ -16,35 +13,35 @@ const Detail = (articleNo, dataUrl) => {
     });
 
     const contents = (article) => {
-      return `
-                <article class="article__box">
-                    <header class="article__header">
-                        <img class="header__img" src="${article.thumbnail}"/>
-                        <h1 class="header__title">${article.title}</h1>
-                        <section class="header__section">
-                            <img src="https://static.toss.im/assets/toss-tech/slash_juneseokbeomgeun.png" alt=""/>
-                            <div class="profile__wrap">
-                                <h3>이준석/송범근  iOS Developer</h3>
-                                <p>${article.date}</p>
-                            </div>
-                        </section>
-                    </header>
-                    <div class="article__contents">
-                        <h2 class="contents__introduction">서론</h2>
-                    </div>
-                    <div class="vote__box">
-                        <h2 class="vote__title">재미있게 읽으셨나요?</h2>
-                        <p class="vote__phrase">좋았는지, 아쉬웠는지, 아래 이모지를 눌러 의견을 들려주세요.</p>
-                        <div class="vote__btns">
-                            <div class="vote__btn">😍</div>
-                            <div class="vote__btn">🤔</div>
-                        </div>
-                        <div class="share__btn">
-                            <button>아티클 공유하기</button>
-                        </div>
-                    </div>
-                </article>
-        `;
+      return /*template*/`
+        <article class="article__box">
+          <header class="article__header">
+            <img class="header__img" src="${article.thumbnail}"/>
+            <h1 class="header__title">${article.title}</h1>
+            <section class="header__section">
+              <img src="https://static.toss.im/assets/toss-tech/slash_juneseokbeomgeun.png" alt=""/>
+              <div class="profile__wrap">
+                <h3>이준석/송범근  iOS Developer</h3>
+                <p>${article.date}</p>
+              </div>
+            </section>
+          </header>
+          <div class="article__contents">
+            <h2 class="contents__introduction">서론</h2>
+          </div>
+          <div class="vote__box">
+            <h2 class="vote__title">재미있게 읽으셨나요?</h2>
+            <p class="vote__phrase">좋았는지, 아쉬웠는지, 아래 이모지를 눌러 의견을 들려주세요.</p>
+            <div class="vote__btns">
+              <div class="vote__btn">😍</div>
+              <div class="vote__btn">🤔</div>
+            </div>
+            <div class="share__btn">
+              <button>아티클 공유하기</button>
+            </div>
+          </div>
+        </article>
+      `;
     };
 
     const articleWrap = document.querySelector(".article__wrap");
--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -1,11 +1,40 @@
 import Detail from "./Detail.js";
+// to be lazy....
+const anchorStyle = `
+  .styled-anchor {
+    color: rgb(132,141,75);
+    text-decoration: none;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+  .styled-anchor:hover {
+    color: rgb(0,0,238);
+  }
+`;
+const style = document.createElement('style');
+style.textContent = anchorStyle;
+document.head.appendChild(style);
+// 123
+const StyledAnchor = ({ text }) => {
+  const $styledAnchor = document.createElement('a');
+  $styledAnchor.classList.add('styled-anchor');
+  $styledAnchor.style.cssText = anchorStyle;
+  $styledAnchor.textContent = text;
+
+  return $styledAnchor;
+}
+
+const mainElement = document.querySelector("#main");
+mainElement.appendChild(StyledAnchor({ text: "개발" }));
 
 const MainContent = (page) => {
   const fetchDataAndRendering = async () => {
     let dataUrl = page === "tech" ? "techData" : "designData";
 
     try {
-      const json = await (await fetch(`../../json/${dataUrl}.json`)).json();
+      const json = await (await fetch(`json/${dataUrl}.json`)).json();
       renderListBox(json, dataUrl);
     } catch (err) {
       console.log(err);
@@ -37,20 +66,18 @@ const MainContent = (page) => {
   const renderListBox = (data, dataUrl) => {
     const listUl = document.querySelector(".list__ul");
     const listBox = data.map(
-      (item) =>
-        `
+      (item) => /*template*/`
         <a class="list__anchor">
-              <div class="list__box">
-                  <img class="card__img" data-no='${item.no}' src="${item.thumbnail}" alt="thumbnail"  />
-                  <div class="card__contents">
-                      <h1 class="card__title"  data-no='${item.no}'>${item.title}</h1>
-                      <p class="card__description" data-no='${item.no}'>${item.description}</p>
-                      <p class="card__date" data-no='${item.no}'>${item.date}</p>
-                  </div>
-              </div>
+          <div class="list__box">
+            <img class="card__img" data-no='${item.no}' src="${item.thumbnail}" alt="thumbnail"  />
+            <div class="card__contents">
+              <h1 class="card__title"  data-no='${item.no}'>${item.title}</h1>
+              <p class="card__description" data-no='${item.no}'>${item.description}</p>
+              <p class="card__date" data-no='${item.no}'>${item.date}</p>
+            </div>
+          </div>
         </a>
-      `
-    );
+      `);
 
     listUl.innerHTML = listBox.join("");
 
