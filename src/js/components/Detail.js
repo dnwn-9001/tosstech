093fe7f0b7ed0064e491f0cfeda4ab70435a81e6
dnwn-9001 <j10511@hanmail.net>
2023-09-09 13:07:31 +0900

    컴포넌트 분리, DOM API로 엘리먼트 생성

--- src/js/components/Detail.js
+++ src/js/components/Detail.js
@@ -1,7 +1,12 @@
-// import styled from "styled-components";
-// const articlelWrap = styled.div``;
-
 const Detail = (articleNo, dataUrl) => {
+  const articleWrapStyle = `
+      width: 92%;
+      height: 100%;
+      max-width: 700px;
+    `;
+  const $articleWrap = document.createElement("div");
+  $articleWrap.style.cssText = articleWrapStyle;
+
   const getData = async (articleNo) => {
     try {
       const json = await (await fetch(`json/${dataUrl}.json`)).json();
@@ -10,52 +15,100 @@ const Detail = (articleNo, dataUrl) => {
       console.log(err);
     }
   };
+
   const renderDetail = (data, articleNo) => {
     const article = data.find((item) => {
       return Number.parseInt(item.no) === Number.parseInt(articleNo);
     });
 
-    const contents = (article) => {
-      return `
-                <article class="article__box">
-                    <header class="article__header">
-                        <img class="header__img" src="${article.thumbnail}"/>
-                        <h1 class="header__title">${article.title}</h1>
-                        <section class="header__section">
-                            <img src="https://static.toss.im/assets/toss-tech/slash_juneseokbeomgeun.png" alt=""/>
-                            <div class="profile__wrap">
-                                <h3>이준석/송범근  iOS Developer</h3>
-                                <p>${article.date}</p>
-                            </div>
-                        </section>
-                    </header>
-                    <div class="article__contents">
-                        <h2 class="contents__introduction">서론</h2>
-                    </div>
-                    <div class="vote__box">
-                        <h2 class="vote__title">재미있게 읽으셨나요?</h2>
-                        <p class="vote__phrase">좋았는지, 아쉬웠는지, 아래 이모지를 눌러 의견을 들려주세요.</p>
-                        <div class="vote__btns">
-                            <div class="vote__btn">😍</div>
-                            <div class="vote__btn">🤔</div>
-                        </div>
-                        <div class="share__btn">
-                            <button>아티클 공유하기</button>
-                        </div>
-                    </div>
-                </article>
+    const createElementAndStyle = (article) => {
+      const $articleHeader = document.createElement("header");
+      const headerStyle = `
+          width: 100%;
         `;
+      $articleHeader.style.cssText = headerStyle;
+
+      const $headerImg = document.createElement("img");
+      const headerImgStyle = `
+          width: 100%;
+          margin-top: 36px;
+          border-radius: 12px;
+        `;
+      $headerImg.style.cssText = headerImgStyle;
+      $headerImg.src = article.thumbnail;
+
+      const $h1 = document.createElement("h1");
+      const h1Style = `
+          margin-top: 20px;
+          font-size: 48px;
+          line-height: 1.25;
+          color: rgb(51, 61, 75);
+          word-break: keep-all;
+          overflow-wrap: break-word;
+        `;
+      $h1.style.cssText = h1Style;
+      $h1.textContent = article.title;
+
+      const $headerSection = document.createElement("section");
+      const sectionStyle = `
+          display: flex;
+          width: 100%;
+          margin-top: 20px;
+        `;
+      $headerSection.style.cssText = sectionStyle;
+
+      const $img = document.createElement("img");
+      const imgStyle = `
+          width: 48px;
+          height: 48px;
+          border-radius: 156px;
+          margin-right: 14px;
+        `;
+      $img.style.cssText = imgStyle;
+      $img.src = article.photo;
+
+      const $h3 = document.createElement("h3");
+      const h3Style = `
+          margin: 0;
+          font-size: 17px;
+          font-weight: 600;
+          color: rgb(78, 89, 104);
+          white-space: nowrap;      
+        `;
+      $h3.style.cssText = h3Style;
+      $h3.textContent = article.name;
+
+      const $p = document.createElement("p");
+      const pStyle = `
+          margin: 0;
+          font-size: 14px;
+          line-height: 1.5;
+          color: rgb(139, 149, 161);
+        `;
+
+      $p.style.cssText = pStyle;
+      $p.textContent = article.date;
+
+      const $profileWrapDiv = document.createElement("div");
+      $profileWrapDiv.appendChild($h3);
+      $profileWrapDiv.appendChild($p);
+
+      $headerSection.appendChild($img);
+      $headerSection.appendChild($profileWrapDiv);
+
+      $articleHeader.appendChild($headerImg);
+      $articleHeader.appendChild($h1);
+      $articleHeader.appendChild($headerSection);
+
+      return $articleHeader;
     };
 
-    const articleWrap = document.querySelector(".article__wrap");
-    articleWrap.innerHTML = contents(article);
+    $articleWrap.appendChild(createElementAndStyle(article));
   };
 
   getData(articleNo);
 
-  return `
-  <div class="article__wrap"></div>
-  `;
+  return $articleWrap;
 };
 
 export default Detail;
